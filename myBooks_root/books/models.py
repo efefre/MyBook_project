ddd481b9eab712abcb3cbd812d9167553386e4ca
Kasia <efefre@icloud.com>
2019-06-22 12:44:45 +0200

    Create model: Book

--- myBooks_root/books/models.py
+++ myBooks_root/books/models.py
@@ -8,3 +8,10 @@ class Author(models.Model):
 class Category(models.Model):
     categoryName = models.CharField(max_length=250)
 
+class Book(models.Model):
+    title = models.CharField(max_length=500)
+    author = models.ManyToManyField(Author)
+    category = models.ManyToManyField(Category)
+    description = models.TextField(blank=True)
+    averageRaiting = models.DecimalField(max_digits=3, decimal_places=1)
+    canonicalVolumeLink = models.CharField(max_length=250)
