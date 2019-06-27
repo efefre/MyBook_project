566d28e2a83ce45d230b28273c1675bb733ac260
Kasia <efefre@icloud.com>
2019-06-27 22:29:35 +0200

    Create function: showImportFromGb witch show div

--- myBooks_root/myBooks_site/static/js/main.js
+++ myBooks_root/myBooks_site/static/js/main.js
@@ -1,2 +1,9 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+function showImportFromGb() {
+    document.getElementById("add_book_form").style.display = "none";
+    document.getElementById("import_from_gb").style.display = "block";
+}
+
+}
