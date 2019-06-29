63a3caecafbfa88d42b0a63651be85a070d9c293
Kasia <efefre@icloud.com>
2019-06-29 11:43:27 +0200

    Update model: Book. Add blank=True to category, averageRaiting and canonicalVolumneLink

--- myBooks_root/books/models.py
+++ myBooks_root/books/models.py
@@ -17,7 +17,12 @@ class Category(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=500)
     author = models.ManyToManyField(Author)
-    category = models.ManyToManyField(Category)
+    category = models.ManyToManyField(Category, blank=True)
     description = models.TextField(blank=True)
-    averageRaiting = models.DecimalField(max_digits=3, decimal_places=1)
-    canonicalVolumeLink = models.CharField(max_length=250)
+    averageRaiting = models.DecimalField(max_digits=3, decimal_places=1, blank=True)
+    canonicalVolumeLink = models.CharField(max_length=250, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
