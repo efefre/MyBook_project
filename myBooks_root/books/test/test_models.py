82b0b20f184de22753982b277f054d61d861f781
Kasia <efefre@icloud.com>
2019-06-30 13:03:37 +0200

    TEST: Create new category

--- myBooks_root/books/test/test_models.py
+++ myBooks_root/books/test/test_models.py
@@ -1,4 +1,4 @@
-from ..models import Author
+from ..models import Author, Category
 import pytest
 
 @pytest.mark.django_db
@@ -6,3 +6,9 @@ def test_create_author():
     author = Author.objects.create(fullName="Gall Anonim")
     author.save()
     assert author.fullName == "Gall Anonim"
+
+@pytest.mark.django_db
+def test_create_category():
+    category = Category.objects.create(categoryName="Pierwsza kategoria")
+    category.save()
+    assert category.categoryName == "Pierwsza kategoria"
