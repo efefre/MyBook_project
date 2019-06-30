4e0eabf99f315d127ac97edc520ab7fe04cf23b3
Kasia <efefre@icloud.com>
2019-06-30 11:51:54 +0200

    Create first version of view for adding book by form.
    1 - check if author exist in database adn if yes: check if title exist in database;
    2 - save new book in database

--- myBooks_root/books/views.py
+++ myBooks_root/books/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book, Author, Category
 
 # Create your views here.
 def index(request):
@@ -6,3 +7,50 @@ def index(request):
 
 def add_book(request):
     return render(request, 'books/add_book.html')
+
+def new_book(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        author = request.POST['author']
+        category = request.POST['category']
+        description = request.POST['description']
+        averageRaitnig = request.POST['averageRaitnig']
+        link = request.POST['link']
+
+        if not averageRaitnig:
+            averageRaitnig = 0.0
+
+        book = Book(title=title,
+                    description=description,
+                    averageRaiting=averageRaitnig,
+                    canonicalVolumeLink=link)
+
+        category = Category.objects.create(categoryName=category)
+
+
+        # Check if author exist in database
+        author_in_database = Author.objects.all().filter(fullName=author).values('id')
+
+        if author_in_database:
+            author = author_in_database[0]['id']
+            book_in_database = Book.objects.all().filter(title=title, author=author)
+
+            # Check if book exist in database
+            if book_in_database:
+                print('Książka istnieje w bazie danych')
+                return redirect('/add')
+            else:
+                book.save()
+                book.category.add(category)
+                book.author.add(author)
+
+                print('Dodano książkę do bazy danych')
+                return redirect('/')
+        else:
+            book.save()
+            author = Author.objects.create(fullName=author)
+            book.author.add(author)
+            book.category.add(category)
+
+            print('Dodano książkę do bazy danych')
+            return redirect('/')
