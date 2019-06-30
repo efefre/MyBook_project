4cc324de892d2e6d6bab8f07cc4b36e2d340ca95
Kasia <efefre@icloud.com>
2019-06-30 13:23:41 +0200

    TEST: Create book

--- myBooks_root/books/test/test_models.py
+++ myBooks_root/books/test/test_models.py
@@ -1,5 +1,6 @@
 from ..models import Author, Category
 import pytest
+from mixer.backend.django import mixer
 
 @pytest.mark.django_db
 def test_create_author():
@@ -12,3 +13,12 @@ def test_create_category():
     category = Category.objects.create(categoryName="Pierwsza kategoria")
     category.save()
     assert category.categoryName == "Pierwsza kategoria"
+
+@pytest.mark.django_db
+def test_create_book():
+    author = Author.objects.create(fullName="Gall Anonim")
+    author.save()
+    category = Category.objects.create(categoryName="Pierwsza kategoria")
+    category.save()
+    book = mixer.blend('books.Book', category = category, author = author, title="Książka testowa")
+    assert book.title == "Książka testowa"
