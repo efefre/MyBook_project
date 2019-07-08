d837997bab1ec86b0ab1cbf5e215a5730270277d
Kasia <efefre@icloud.com>
2019-07-08 18:26:59 +0200

    TEST: Add book for tests

--- myBooks_root/books/test/test_views.py
+++ myBooks_root/books/test/test_views.py
@@ -2,12 +2,14 @@ from django.urls import reverse
 from django.contrib.messages import get_messages
 import pytest
 
+
 book_for_test = {'title':'Python',
                  'author':'Junior Python',
-                 'category':'ID',
-                 'description':'Opis',
-                 'averageRaitnig':4.0,
-                 'link':'http://python.puniorpython.test'}
+                 'category':'',
+                 'description':'',
+                 'averageRaitnig':'',
+                 'link':''}
+
 
 @pytest.mark.django_db
 def test_add_new_book_by_form(client):
