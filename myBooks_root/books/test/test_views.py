e6482702bcf6f41148ae1cfa667a2ca99f1a7717
Kasia <efefre@icloud.com>
2019-07-08 18:28:34 +0200

    TEST: Add same book twice by form

--- myBooks_root/books/test/test_views.py
+++ myBooks_root/books/test/test_views.py
@@ -20,3 +20,12 @@ class TestAddingBookByForm:
         messages = [m.tags for m in get_messages(response.wsgi_request)]
         assert messages[0] == 'success'
 
+    def test_add_same_book_twice_by_form(self,client):
+        client.post(reverse('books:new_book'),
+                    book_for_test)
+
+        response = client.post(reverse('books:new_book'),
+                               book_for_test)
+
+        messages = [m.tags for m in get_messages(response.wsgi_request)]
+        assert messages[1] == 'danger'
