bff99c964af5613fff99b54009dd03a1f63002c1
Kasia <efefre@icloud.com>
2019-07-09 18:34:43 +0200

    Add path for search

--- myBooks_root/books/urls.py
+++ myBooks_root/books/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.index, name = 'index'),
     path('add', views.add_book, name ='add_book'),
     path('new-book', views.new_book, name='new_book'),
+    path('search', views.search, name='search'),
 ]
