4c95f9e1cf371883018cddf72d9576dd36881f11
Kasia <efefre@icloud.com>
2019-07-21 18:14:00 +0200

    Use new function save_in_database to add book by form

--- myBooks_root/books/views.py
+++ myBooks_root/books/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Book, Author, Category
+from .utils import save_in_database
 import requests
 
 # Create your views here.
@@ -24,48 +25,9 @@ def new_book(request):
         averageRaitnig = request.POST['averageRaitnig']
         link = request.POST['link']
 
-        if not averageRaitnig:
-            averageRaitnig = 0.0
+        save_in_database(request, author, title, category, description, averageRaitnig, link)
 
-        book = Book(title=title,
-                    description=description,
-                    averageRaiting=averageRaitnig,
-                    canonicalVolumeLink=link)
-
-        category_in_database = Category.objects.all().filter(categoryName=category).values('id')
-
-        if category_in_database:
-            category = category_in_database[0]['id']
-        else:
-            category = Category.objects.create(categoryName=category)
-
-
-        # Check if author exist in database
-        author_in_database = Author.objects.all().filter(fullName=author).values('id')
-
-        if author_in_database:
-            author = author_in_database[0]['id']
-            book_in_database = Book.objects.all().filter(title=title, author=author)
-
-            # Check if book exist in database
-            if book_in_database:
-                messages.error(request, 'Książka istnieje w bazie danych')
-                return redirect('/add')
-            else:
-                book.save()
-                book.category.add(category)
-                book.author.add(author)
-
-                messages.success(request, 'Książka została dodana do bazy danych. Możesz dodać kolejną książkę :)')
-                return redirect('/add')
-        else:
-            book.save()
-            author = Author.objects.create(fullName=author)
-            book.author.add(author)
-            book.category.add(category)
-
-            messages.success(request, 'Książka została dodana do bazy danych. Możesz dodać kolejną książkę :)')
-            return redirect('/add')
+    return redirect('/add')
 
 def search(request):
     books = Book.objects.order_by('title')
